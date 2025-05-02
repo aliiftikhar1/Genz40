449f55dc2613f1a55abe9a2cd74e9c33a997689b
aliiftikhar1 <aj829077@gmail.com>
2025-05-02 20:11:33 +0500

    Update asgi.py

--- genz40/asgi.py
+++ genz40/asgi.py
@@ -6,16 +6,23 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
+
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
 
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'genz40.settings')
 
+# Initialize Django app registry
+django_asgi_app = get_asgi_application()
+
+# Import chat.routing *after* Django is initialized
+from chat.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
